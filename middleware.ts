140b5c2007001be3d9924399de8b31a563935d0b
URTheCoolest <Jobangles4life@gmail.com>
2025-12-11 20:20:15 +0100

    fix #8

--- middleware.ts
+++ middleware.ts
@@ -8,7 +8,7 @@ export async function middleware(req: NextRequest) {
   // Adapter to let Supabase read/write cookies in middleware
   const supabase = createServerClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+    process.env.SUPABASE_SERVICE_ROLE_KEY!,
     {
       cookies: {
         get: (name: string) => req.cookies.get(name)?.value,
